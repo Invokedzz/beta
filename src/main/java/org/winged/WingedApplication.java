e26bde553c4f8ea41d466ee25637deb0c6a71405
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-03-28 18:29:53 -0300

    starting project :)

---
+++ src/main/java/org/winged/WingedApplication.java
@@ -0,0 +1,13 @@
+package org.winged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WingedApplication.class, args);
+    }
+
+}
